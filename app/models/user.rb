abc2cdefd796d7655c1dadde097f5cfa022fc0bb
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-15 22:14:06 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,21 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :nickname, presence: true
+  validates :email, uniqueness: true #一意性であること
+  validates :birthday, presence: true
+  validates :password, confirmation: true #パスワードは、確認用を含めて2回入力すること
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :family_name
+    validates :first_name_kana
+    validates :family_name_kana
+  end
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください' 
+  #上記２行でパスワードは、半角英数字混合での入力が必須であることの実装
 end
+
