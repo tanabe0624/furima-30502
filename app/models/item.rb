207d4a2c98dfb8ed0d58255792017ce21b7af6db
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-16 17:58:35 +0900

    バリデーションの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,15 @@ class Item < ApplicationRecord
 
   belongs_to :user
   has_one_attached :image
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category_id, presence: true
+  validates :condition_id, presence: true
+  validates :price, presence: true
+  validates :postage_id, presence: true
+  validates :region_id, presence: true
+  validates :shipping_date_id, presence: true
+  validates :image, presence: true
+  
 end
