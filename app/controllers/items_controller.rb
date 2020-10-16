ffd77d1d9b8e7bef5dd1bbe39a3270a18c64551a
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-16 18:01:24 +0900

    アクションとルーティングの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -3,6 +3,12 @@ class ItemsController < ApplicationController
   def index
   end
 
+  def new
+  end
+
+  def create
+  end
+
   private
   def item_params
     params.require(:item).permit(:image).merge(user_id: current_user.id)
