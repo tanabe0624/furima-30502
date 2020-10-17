d089037a2b7462c493af070aeb4d2d33d9b3fd15
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-17 21:04:52 +0900

    各アクションの処理内容記載

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,14 +4,21 @@ class ItemsController < ApplicationController
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
   end
 
   private
   def item_params
-    params.require(:item).permit(:image).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :postage_id, :region_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 
 end
