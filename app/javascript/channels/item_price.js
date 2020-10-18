f2c1af94966a3f8581af102ee1d95640302b2d5b
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-18 13:06:52 +0900

    デバック用の記述削除

--- app/javascript/channels/item_price.js
+++ app/javascript/channels/item_price.js
@@ -1,11 +1,8 @@
 window.addEventListener('load', () => {
-  console.log("OK");
-
   //constも発火条件が必要
   const priceInput = document.getElementById("item-price");
   priceInput.addEventListener("input", () => {
     const inputValue = priceInput.value;
-    console.log(inputValue);
 
     const addTaxDom = document.getElementById("add-tax-price")
     addTaxDom.innerHTML = Math.floor(inputValue * 0.1);
@@ -14,6 +11,5 @@ window.addEventListener('load', () => {
     profitDom.innerHTML = Math.floor(inputValue * 0.9);
 
   })
-  
 });
 
