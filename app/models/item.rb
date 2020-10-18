06ad7ec7bc70eb65441d432d5ae8e8a330c8caeb
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-18 11:53:00 +0900

    バリデーションの修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,16 +1,23 @@
 class Item < ApplicationRecord
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to_active_hash :category
+  belongs_to_active_hash :condition
+  belongs_to_active_hash :postage
+  belongs_to_active_hash :region
+  belongs_to_active_hash :shipping_date
+
   belongs_to :user
   has_one_attached :image
+  
+  validates :name, :description, :category_id, :condition_id, :price, :postage_id, :region_id, :shipping_date_id, :image, presence: true
+  validates :category_id, :condition_id, :postage_id, :region_id, :shipping_date_id, numericality: { other_than: 1 }
+  #このバリデーションは、各idのid:1以外のときに保存できるという意味
 
-  validates :name, presence: true
-  validates :description, presence: true
-  validates :category_id, presence: true
-  validates :condition_id, presence: true
-  validates :price, presence: true
-  validates :postage_id, presence: true
-  validates :region_id, presence: true
-  validates :shipping_date_id, presence: true
-  validates :image, presence: true
+  validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to: 9999999}
+  VALID_PRICE_REGEX = /\A[\d]+\z/i
+  validates :price, format: { with: VALID_PRICE_REGEX }
+
   
 end
+
