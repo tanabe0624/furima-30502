2b1713592d079e7448ad701fa5fc305631fe57d5
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-18 11:52:24 +0900

    itemモデルのテストコード作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,19 @@
 FactoryBot.define do
   factory :item do
-    
+
+    name {Faker::Name.name}
+    description {Faker::Lorem.sentence}
+    category_id {"2"}
+    condition_id {"2"}
+    postage_id {"2"}
+    region_id {"2"}
+    shipping_date_id {"2"}
+    price {"1000"}
+
+    association :user
+
+    after(:build) do |item|
+      item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+    end
   end
 end
