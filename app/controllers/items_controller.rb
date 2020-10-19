2a9f332400240c884de5201a7908177699fab217
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-19 21:50:59 +0900

    空になったアクションを削除、move_to_actionにupdate追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,7 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:edit, :show, :update]
   before_action :authenticate_user!, except: [:index,:show,]
-  before_action :move_to_index, only: :edit
+  before_action :move_to_index, only: [:edit, :update]
 
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
@@ -20,11 +20,6 @@ class ItemsController < ApplicationController
     end
   end
 
-  def show
-  end
-
-  def edit
-  end
 
   def update
     if @item.update(item_params)
