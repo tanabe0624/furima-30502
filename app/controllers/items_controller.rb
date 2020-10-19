ef602fa864f4ef5eacd80f8eaa171d21015c5541
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-19 21:30:47 +0900

    同じ記述をメソッドにまとめる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
-  before_action :authenticate_user!, except: [:index,:show]
+  before_action :set_item, only: [:edit, :show, :update]
+  before_action :authenticate_user!, except: [:index,:show,]
   before_action :move_to_index, only: :edit
 
   def index
@@ -20,15 +21,12 @@ class ItemsController < ApplicationController
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def update
-    @item = Item.find(params[:id])
     if @item.update(item_params)
       redirect_to item_path(@item.id)
     else
@@ -46,5 +44,9 @@ class ItemsController < ApplicationController
     unless user_signed_in? && current_user.id == @item.user.id
       redirect_to action: :index
     end
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
