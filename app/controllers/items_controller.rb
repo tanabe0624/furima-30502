637c4721c9c44bbfc0fa8f3cdae1f17291005f02
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-10-19 20:34:36 +0900

    各アクションの処理設定、move_to_indexメソッドの設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,7 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index,:show]
+  before_action :move_to_index, only: :edit
+
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
   end
@@ -21,9 +23,28 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
+    else
+      render :edit
+    end
+  end
+
   private
   def item_params
     params.require(:item).permit(:name, :description, :category_id, :condition_id, :price, :postage_id, :region_id, :shipping_date_id, :image).merge(user_id: current_user.id)
   end
 
+  def move_to_index
+    @item = Item.find(params[:id])
+    unless user_signed_in? && current_user.id == @item.user.id
+      redirect_to action: :index
+    end
+  end
 end
