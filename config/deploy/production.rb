deb2aac9080e9356121c949a787a61bb515b0582
tanabe0624 <ryo.tnb.bn@gmail.com>
2020-11-11 16:36:57 +0900

    Elastic　IPアドレスの変更

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -59,4 +59,4 @@
 #     auth_methods: %w(publickey password)
 #     # password: "please use keys"
 #   }
-server '54.250.108.82', user: 'ec2-user', roles: %w{app db web}
+server '54.248.47.25', user: 'ec2-user', roles: %w{app db web}
